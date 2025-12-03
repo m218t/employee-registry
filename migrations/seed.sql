f5e7e8a705e59a1aa9306592aeca1f9fec752c5d
MarkTeryokhin <maaqicyk@gmail.com>
2025-12-03 20:26:02 +0300

    feat: add seed data for employees table

---
+++ migrations/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (last_name, first_name, middle_name, birth_date, passport_series, passport_number, phone, email, address, department, position, salary, hire_date)
+VALUES ('Иванов','Иван','Иваныч','1990-01-10','1234','567890','+7 (999) 111-22-33','ivan@example.com','г. Город, ул. Дом 1','IT','QA',80000,'2023-06-01');
