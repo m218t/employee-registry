0b6743f2925f3974566b9c1c7e07802ec0cd6643
MarkTeryokhin <maaqicyk@gmail.com>
2025-12-06 02:32:39 +0300

    feat: implement REST API for employees management

--- api/src/employees/employees.controller.ts
+++ api/src/employees/employees.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Get, Post, Put, Patch, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Param, Body, Query } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
-import { Employee } from '../employee.entity';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) { }
 
   @Get()
-  getAll() {
-    return this.employeesService.findAll();
+  getAll(@Query('department') department?: string, @Query('position') position?: string) {
+    return this.employeesService.findAll(department, position);
+  }
+
+  @Get('search')
+  search(@Query('name') name: string) {
+    return this.employeesService.searchByName(name);
+  }
+
+  @Get('departments')
+  getDepartments() {
+    return this.employeesService.getDepartments();
+  }
+
+  @Get('positions')
+  getPositions() {
+    return this.employeesService.getPositions();
   }
 
   @Get(':id')
@@ -17,17 +33,17 @@ export class EmployeesController {
   }
 
   @Post()
-  create(@Body() employee: Partial<Employee>) {
-    return this.employeesService.create(employee);
+  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+    return this.employeesService.create(createEmployeeDto);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() data: Partial<Employee>) {
-    return this.employeesService.update(id, data);
+  update(@Param('id') id: number, @Body() updateEmployeeDto: UpdateEmployeeDto) {
+    return this.employeesService.update(id, updateEmployeeDto);
   }
 
-  @Patch(':id/fire')
-  fire(@Param('id') id: number) {
-    return this.employeesService.fire(id);
+  @Patch(':id/dismiss')
+  dismiss(@Param('id') id: number) {
+    return this.employeesService.dismiss(id);
   }
 }
